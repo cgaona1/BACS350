94f15b4b91b1e4b13d88676b19926037251273d3
cgaona1 <gaonachris930@gmail.com>
2021-09-02 09:55:39 -0600

    Added testPageView

--- week1/Hello/pages/views.py
+++ week1/Hello/pages/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homePageView(request):
-    return HttpResponse('Hello World!!!, mistake fixed')
+    return HttpResponse("<h1>Hello World!!!</h1><p>Mistake fixed</p>")
+
+def testPageView(request):
+    return HttpResponse("<h1>This is a test</h1>" + \
+        "<p>Testing new view \"testPageView\" ")
