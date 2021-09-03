b048ec199f7f8db3cbb8074df991a242ace749f0
cgaona1 <gaonachris930@gmail.com>
2021-09-03 15:52:59 -0600

    pages.html added and app update

--- week2/Hello/pages/urls.py
+++ week2/Hello/pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('about/', views.AboutPageView.as_view(), name='about'),
+    path('testPage/', views.PageView.as_view(), name='page'),
 ]
--- week2/Hello/pages/views.py
+++ week2/Hello/pages/views.py
@@ -6,3 +6,12 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class PageView(TemplateView):
+    template_name = 'page.html'
+
+    def get_context_data(self, **kwargs):
+        return{
+            'title': 'Test data as dictionary',
+            'body': 'This is a test for title and body data',
+        }
