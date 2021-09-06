65b795446b69b8ede33793567f62c954db12452f
cgaona1 <gaonachris930@gmail.com>
2021-09-06 00:10:18 -0600

    added home page and fixed image loading problems

--- week2/Superhero/hero/views.py
+++ week2/Superhero/hero/views.py
@@ -1,16 +1,22 @@
 from django.views.generic import TemplateView
 
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
 class SpidermanView(TemplateView):
     template_name = 'spiderman.html'
 
+    def get_context_data(self, **kwargs):
+        return{
+            'hero_name': 'spiderman'
+        }
+
 class BabyGrootView(TemplateView):
     template_name = 'babygroot.html'
 
-    '''def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):
         return{
-            'title': 'Test data as dictionary',
-            'body': 'This is a test for title and body data',
+            'hero_name': 'babygroot'
         }
-    '''
+    
