a89ec95b8dddbda4212e1c68225e278bb6836667
cgaona1 <gaonachris930@gmail.com>
2021-09-22 13:54:30 -0600

    Hero detail template created

--- week4/Superhero/hero/views.py
+++ week4/Superhero/hero/views.py
@@ -2,8 +2,19 @@ from django.views.generic import ListView, TemplateView
 from . import models
 
 # Create your views here.
+
+
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class HeroDetailView(TemplateView):
+    model = models.Hero
+    template_name = 'hero_detail.html'
+
+    def get_context_data(self, **kwargs):
+        return {'hero': models.Hero.objects.get(pk=1)}
+
 
 class HeroListView(ListView):
     model = models.Hero
