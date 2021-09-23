d6873b6aaf79b9803d758b20fb0b114e26df52dc
cgaona1 <gaonachris930@gmail.com>
2021-09-23 12:57:22 -0600

    Test update

--- week4/Superhero/hero/tests.py
+++ week4/Superhero/hero/tests.py
@@ -2,7 +2,8 @@ from django.http import response
 from django.test import SimpleTestCase
 from django.test import TestCase
 
-# Create your tests here.
+'''
+# Create your simple tests here.
 class SimpleTests(SimpleTestCase):
     def test_home_page_status_code(self):
         response = self.client.get('/')
@@ -11,8 +12,19 @@ class SimpleTests(SimpleTestCase):
     def test_admin_page_status_code(self):
         response = self.client.get('/admin/')
         self.assertEqual(response.status_code, 302)
+'''
+# Create your test here
+
 
 class Tests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_page_status_code(self):
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 302)
+
     def test_hero_page_status_code(self):
         response = self.client.get('/hero/')
         self.assertEqual(response.status_code, 200)
