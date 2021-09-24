4b4bb6090fce1a00c63ef128b463148ca5dd06ee
cgaona1 <gaonachris930@gmail.com>
2021-09-24 15:06:47 -0600

    Added templates and updated urls

--- week5/Superhero/hero/views.py
+++ week5/Superhero/hero/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, ListView, TemplateView
+from . import models
 
 # Create your views here.
 
 
-class HeroView(TemplateView):
-    template_name = 'index.html'
+class HeroView(ListView):
+    template_name = 'hero_list.html'
+    model = models.Hero
+
+
+class HeroDetailView(DetailView):
+    template_name = 'hero_detail.html'
+    model = models.Hero
